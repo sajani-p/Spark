bfb16ac196274b87a45cc51624cba2b47888f830
sajani pathegama <61696200+sajani-p@users.noreply.github.com>
2021-10-23 20:47:06 +0530

    Add files via upload

---
+++ Frontend/src/shared/utils.js
@@ -0,0 +1,13 @@
+export function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export function getTodaysDate() {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2,"0");
+  let mm = String(today.getMonth()).padStart(2,"0");
+  let yyyy = today.getFullYear();
+
+  today = mm + "/" + dd + "/" + yyyy;
+  return today;
+}
